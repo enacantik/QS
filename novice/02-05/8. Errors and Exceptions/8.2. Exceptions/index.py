34115496cfe1071a254650b0230517ad9e53e8e2
enimanis <sitieniaisyahsbln1000@gmail.com>
2022-08-25 11:58:37 -1200

    update teks

---
+++ novice/02-05/8. Errors and Exceptions/8.2. Exceptions/index.py
@@ -0,0 +1,12 @@
+10 * (1/0)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  print()
+4 + spam*3
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+
+'2' + 2
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  
---
+++ novice/02-05/8. Errors and Exceptions/8.4. Raising Exceptions/index.py
@@ -0,0 +1,15 @@
+raise NameError('HiThere')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+
+raise ValueError  # shorthand for 'raise ValueError()'
+
+try:
+...     raise NameError('HiThere')
+... except NameError:
+...     print('An exception flew by!')
+...     raise
+...
+An exception flew by!
+Traceback (most recent call last):
+  File "<stdin>", line 2, in <module>
