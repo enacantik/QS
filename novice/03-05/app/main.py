766cdb9601f4a0c3824034465a4c4d34396dd270
enacantik <sitienaaisyahsbln1000@gmail.com>
2022-09-05 12:02:44 +0700

    update teks

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -35,6 +35,40 @@ def index():
     conn.close()
     # data =["pisang", "jambu", "durian"]
     return render_template("index.html", context=data)
+
+# @app.route("/detail/<buah_id>")
+# def detail(buah_id):
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         database="contoh",
+#         user="postgres",
+#         password="enacantiksekali")
+
+#     curs = conn.cursor()
+#     query = f"select * from buah where id = {buah_id}"
+#     curs.execute(query)
+#     data = curs.fetchone()
+#     curs.close()
+#     conn.close()
+#     print(data)
+#     return render_template("detail.html", context=data)
+
+@app.route("/delete/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="enacantiksekali")
+
+    curs = conn.cursor()
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect ("/")
+
 
 if __name__ == "__main__":
     app.run()
